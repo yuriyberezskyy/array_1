79457e1c3f02d0b48a737a36395d0e965942d55a
Yuriy Berezskyy <54465428+yuriyberezskyy@users.noreply.github.com>
2022-10-19 19:37:31 +0000

    Added some classes and created Array Class

--- index.js
+++ index.js
@@ -15,4 +15,73 @@ strings.unshift('x');
 //splice add or delete item in index
 strings.splice(2,1,'alien')
 
+//
 console.log(strings)
+
+class Player {
+		constructor( name, type){
+			this.name = name
+			this.type = type
+    }
+
+    introduce(){
+	  console.log("Hi I am ${this.name}. I am a ${this.type}")
+  }
+}
+
+class Wizard extends Player {
+	  constructor(name,type){
+   	super(name,type)
+  }
+  play(){
+  	console.log("Weeeeee! I am a ${this.type}")
+  }
+}
+
+const wizard1 = new Wizard("Shelly", "Healer");
+
+
+
+class MyArray{
+  constructor(){
+    this.length = 0
+    this.data = {};
+  }
+
+  get(index){
+    return this.data[index]
+  }
+
+  push(item){
+    this.data[this.length] = item
+    this.length++
+    return this.length
+  }
+
+  pop(){
+    const lastItem = this.data[this.length - 1]
+    delete this.data[this.length -1]
+    this.length--;
+    return lastItem
+  }
+
+  delete(index){
+    const item = this.data[index]
+    this.shiftItems(index)
+  }
+
+  shiftItems(index){
+    for(let i = index; i < this.length-1; i++){
+      this.data[i] = this.data[i+1]
+    }
+    delete this.data[this.length-1]
+    this.length--
+  }
+}
+
+const newArray = new MyArray();
+newArray.push('hello')
+newArray.push("you")
+newArray.push("!")
+newArray.delete(0)
+console.log(newArray)
